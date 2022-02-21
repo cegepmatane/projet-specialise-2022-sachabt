4c4cf21d656c3995cdd612c935a336db3c75df41
sachabt <sbelliertoulouzou@gmail.com>
2022-02-20 23:57:31 -0500

    removed useless old code

--- app/ocean/scripts/Save.gd
+++ app/ocean/scripts/Save.gd
@@ -43,14 +43,6 @@ func load_game():
 	
 	#ne prends aussi pas en compte la possibilité d'avoir des objets "Persist" imbriqué
 
-	# We need to revert the game state so we're not cloning objects
-	# during loading. This will vary wildly depending on the needs of a
-	# project, so take care with this step.
-	# For our example, we will accomplish this by deleting saveable objects.
-	var save_nodes = get_tree().get_nodes_in_group("Persist")
-	for i in save_nodes:
-		i.queue_free()
-
 	# Load the file line by line and process that dictionary to restore
 	# the object it represents.
 	save_game.open("user://savegame.save", File.READ)
