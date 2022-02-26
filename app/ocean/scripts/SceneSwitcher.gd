3ae24234c0ae4a8d5a4e17a52c39854b9b8ca9fe
sachabt <sbelliertoulouzou@gmail.com>
2022-02-26 00:44:56 -0500

    #12 testing transition

--- app/ocean/scripts/SceneSwitcher.gd
+++ app/ocean/scripts/SceneSwitcher.gd
@@ -2,4 +2,7 @@ extends Node2D
 
 
 func change_scene(new_scene):
+	$AnimationPlayer.play("fade")
+	yield($AnimationPlayer, "animation_finished")
 	assert(get_tree().change_scene(new_scene)==OK)
+	$AnimationPlayer.play_backwards("fade")
